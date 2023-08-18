769f618486dd5cbc12b4f31053f35d1299543646
Jocelyn <jdupuis@my365.bellevue.edu>
2023-08-17 21:39:54 -0400

    commit

---
+++ week_2/dupuis_myworld.py
@@ -0,0 +1,13 @@
+#-----------------------------------------
+# Title: dupuis_myworld.py
+# Author: Jocelyn Dupuis
+# Date: 08/17/2023
+# Description: First file to test Python
+#-----------------------------------------
+
+
+# Variable declared for my last name 
+last_name = "Dupuis"
+
+# To print text 
+print("You are now in " + last_name +"' world!")
